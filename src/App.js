08c2ab1501907346605b9d1cbd8c61bbeb51fed5
DarryllRobinson <darryll@stillproud.com>
2020-02-20 20:43:36 +1000

    Dev work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import LogState from './Auth/LogState';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      campaign: 1
+    };
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  gotoCard(card) {
+    console.log('gotoCard');
+  }
+
   render() {
     const { isAuthenticated } = this.props.auth;
-
-
 
     return (
       <div>
         {
           isAuthenticated() && (
-            <div>isAuthenticated</div>
+            <div>
+              <p>List of active cards</p>
+              <button onClick={this.gotoCard.bind(this, this.state.campaign)}>Load card</button>
+              <p>List of other cards</p>
+
+              <button onClick={this.logout.bind(this)}>Log out</button>
+            </div>
+          )
+        }
+        {
+          !isAuthenticated() && (
+            <div>
+              <button onClick={this.login.bind(this)}>Log in or sign up here</button>
+            </div>
           )
         }
       </div>
